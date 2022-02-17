a6b290312d29b8197d857f5997f96fce24e21908
Hafiz-Sakib <hafizsakib5@gmail.com>
2022-02-17 17:22:00 +0600

    common function added

--- JS/app.js
+++ JS/app.js
@@ -1,13 +1,13 @@
-document.getElementById("calculate-btn").addEventListener("click", function () {
+function calculateCosts(incomeId, foodId, rentId, clothId) {
   //get  income value
-  const income = document.getElementById("income");
+  const income = document.getElementById(incomeId);
   const incomeText = income.value;
   const incomeAmount = parseFloat(incomeText);
 
   //get input values of expances
-  const foodCost = document.getElementById("food-cost");
-  const rentCost = document.getElementById("rent-cost");
-  const clothsCost = document.getElementById("cloths-cost");
+  const foodCost = document.getElementById(foodId);
+  const rentCost = document.getElementById(rentId);
+  const clothsCost = document.getElementById(clothId);
   const foodCostAmount = parseFloat(foodCost.value);
   const rentCostAmount = parseFloat(rentCost.value);
   const clothsCostAmount = parseFloat(clothsCost.value);
@@ -18,7 +18,53 @@ document.getElementById("calculate-btn").addEventListener("click", function () {
   //set Balance
   const balance = document.getElementById("balance");
   balance.innerText = incomeAmount - totalExpenseAmount;
-  if (totalExpenseAmount > incomeAmount) {
-    document.body.style.backgroundColor = "red";
+
+  if (
+    incomeAmount < 0 ||
+    foodCostAmount < 0 ||
+    rentCostAmount < 0 ||
+    clothsCostAmount < 0
+  ) {
+    document.getElementById("alert-paragraph3").style.display = "block";
+    document.getElementById("alert-paragraph2").style.display = "none";
+    document.getElementById("alert-paragraph1").style.display = "none";
+
+    document.getElementById("balance").innerText = "❗Error❗";
+
+    document.getElementById("total-expenses").innerText =
+      "❗can't expense money❗";
+  } else if (totalExpenseAmount > incomeAmount) {
+    document.getElementById("alert-paragraph1").style.display = "block";
+
+    document.getElementById("alert-paragraph2").style.display = "none";
+
+    document.getElementById("alert-paragraph3").style.display = "none";
+
+    document.getElementById("balance").innerText = "❗Error❗";
+
+    document.getElementById("total-expenses").innerText =
+      "❗can't expense money❗";
+  } else if (
+    isNaN(incomeAmount) == true ||
+    isNaN(foodCostAmount) == true ||
+    isNaN(rentCostAmount) == true ||
+    isNaN(clothsCostAmount) == true
+  ) {
+    document.getElementById("alert-paragraph2").style.display = "block";
+
+    document.getElementById("alert-paragraph1").style.display = "none";
+
+    document.getElementById("alert-paragraph3").style.display = "none";
+
+    document.getElementById("balance").innerText = "❗Error❗";
+
+    document.getElementById("total-expenses").innerText =
+      "❗can't expense money❗";
   }
+}
+
+// add event handler for calculate button
+document.getElementById("calculate-btn").addEventListener("click", function () {
+  //calling function for calculate expenses and  balance
+  calculateCosts("income", "food-cost", "rent-cost", "cloths-cost");
 });
